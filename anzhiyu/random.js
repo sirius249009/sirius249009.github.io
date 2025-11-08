01d2ab137b5c8d0367690d859e6f5f97161da9a2
sirius249009 <2490099566@qq.com>
2025-11-08 21:36:35 +0800

    Site updated: 2025-11-08 21:36:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/10/Arp欺骗/","2024/11/10/NOP Sled空中操作雪橇（代码滑梯）/","2024/11/10/Off-By-One/","2024/11/10/ORW沙箱逃逸/","2024/11/10/不同Shellcode/","2024/11/10/Use-after-free/","2024/11/10/栈迁移/","2024/11/10/静态编译/"];function toRandomPost(){
+var posts=["2024/11/10/Arp欺骗/","2024/11/10/NOP Sled空中操作雪橇（代码滑梯）/","2024/11/10/ORW沙箱逃逸/","2024/11/10/Off-By-One/","2024/11/10/Use-after-free/","2024/11/10/静态编译/","2024/11/10/不同Shellcode/","2024/11/10/栈迁移/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
